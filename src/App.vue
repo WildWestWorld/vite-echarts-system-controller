fe0b84446781d1d0f3d70c88012c12bbbe87bec6
ly <543417822@qq.com>
2022-09-06 13:20:45 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Index from './views/Index.vue';
+</script>
+
+<template>
+  <Index></Index>
+</template>
+
+<style scoped></style>
